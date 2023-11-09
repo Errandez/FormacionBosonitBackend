548982524c4d13140d5309ef38f24f14aea8b502
erick.randez <erickrandezalfaro@gmail.com>
2023-11-09 08:37:49 +0100

    Bloque_12

--- block7-crud-validation/src/main/java/com/example/block7crudvalidation/Block7CrudValidationApplication.java
+++ block7-crud-validation/src/main/java/com/example/block7crudvalidation/Block7CrudValidationApplication.java
@@ -9,7 +9,6 @@ import java.sql.Date;
 
 @SpringBootApplication
 public class Block7CrudValidationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Block7CrudValidationApplication.class, args);
     }
